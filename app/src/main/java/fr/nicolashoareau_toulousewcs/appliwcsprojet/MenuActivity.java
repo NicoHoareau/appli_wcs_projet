1d77508db4e8299dc26279b1d99cdcd998a6ccab
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-04-18 22:53:04 +0200

    menu avec onglets, début request fragment, ajout couleur spécial wcsColor, thème et splashscreen

--- app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/MenuActivity.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/MenuActivity.java
@@ -1,11 +1,14 @@
 package fr.nicolashoareau_toulousewcs.appliwcsprojet;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity implements RequestFragment.OnFragmentInteractionListener, ValidationRequestFragment.OnFragmentInteractionListener, ActualityFragment.OnFragmentInteractionListener{
 
@@ -13,6 +16,15 @@ public class MenuActivity extends AppCompatActivity implements RequestFragment.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        FloatingActionButton addQcm = findViewById(R.id.btn_float_request);
+        addQcm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToCreateRequest= new Intent(MenuActivity.this, CreateRequestActivity.class);
+                MenuActivity.this.startActivity(goToCreateRequest);;
+            }
+        });
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.addTab(tabLayout.newTab().setText(R.string.name_tab1));
