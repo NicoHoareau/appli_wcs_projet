1adeaf38127ec910f1422a01f42222c5d538a26a
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-05-23 12:06:55 +0200

    actulity adptder, item, fragmentlistview et post

--- app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/ActualityAdapter.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/ActualityAdapter.java
@@ -5,6 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -20,12 +25,15 @@ public class ActualityAdapter extends ArrayAdapter<ActualityModel> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_fragment_actuality, parent, false);
         }
 
+        ImageView ivUserPhoto = convertView.findViewById(R.id.iv_userphoto);
+        TextView tvUsernameUser = convertView.findViewById(R.id.tv_username_user);
+        ImageView ivAddPhoto = convertView.findViewById(R.id.iv_photo_added);
+        EditText etDescriptionPhoto = convertView.findViewById(R.id.tv_description_actuality);
 
-
-
-
-
-
+        Glide.with(parent.getContext()).load(actualityModel.getUrlPhotoUser()).into(ivUserPhoto);
+        Glide.with(parent.getContext()).load(actualityModel.getUrlPhoto()).into(ivAddPhoto);
+        tvUsernameUser.setText(actualityModel.getUsername());
+        etDescriptionPhoto.setText(actualityModel.getDescription());
 
         return convertView;
     }
--- app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/ActualityFragment.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/ActualityFragment.java
@@ -1,12 +1,21 @@
 package fr.nicolashoareau_toulousewcs.appliwcsprojet;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
 /**
@@ -89,6 +98,33 @@ public class ActualityFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    FirebaseDatabase mDatabase;
+    DatabaseReference mActualityRef;
+    private String mUid;
+    FloatingActionButton mBtnFloat;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mDatabase = FirebaseDatabase.getInstance();
+
+        ListView listActuality = getView().findViewById(R.id.list_actuality);
+        final ArrayList<ActualityModel> actualityModelArrayList = new ArrayList<>();
+        final ActualityAdapter adapter = new ActualityAdapter(getContext(), actualityModelArrayList);
+        listActuality.setAdapter(adapter);
+
+        mBtnFloat = view.findViewById(R.id.btn_float_actuality);
+        mBtnFloat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToCreateRequest = new Intent(getContext(), CreateRequestActivity.class);
+                startActivity(goToCreateRequest);
+            }
+        });
+
+
+
     }
 
     /**
