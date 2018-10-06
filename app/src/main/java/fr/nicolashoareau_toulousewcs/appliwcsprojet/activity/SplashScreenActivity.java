e1dc3b84920824b8a862a6db27dd28ee1092f416
Perrine <morandperrine@hotmail.fr>
2018-10-06 22:20:04 +0200

    correction alignement design

--- app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/activity/SplashScreenActivity.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/appliwcsprojet/activity/SplashScreenActivity.java
@@ -2,13 +2,14 @@ package fr.nicolashoareau_toulousewcs.appliwcsprojet.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 import fr.nicolashoareau_toulousewcs.appliwcsprojet.R;
 
-public class FirstScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,16 @@ public class FirstScreenActivity extends AppCompatActivity {
         ivClearWindows.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(FirstScreenActivity.this, ConnexionActivity.class);
+                Intent i = new Intent(SplashScreenActivity.this, ConnexionActivity.class);
                 startActivity(i);
+            }
+        });
+
+        ConstraintLayout splashScreen = findViewById(R.id.splash_screen);
+        splashScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashScreenActivity.this, ConnexionActivity.class));
             }
         });
     }
